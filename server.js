843fa994e9c56b7d655223007772bbfc5f8f7172
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-06-07 22:51:11 +0530

    changed port as well as the mongodb url

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ app.use(express.static("public"));
 // express.static() is a middleware that is used to serve static files
 app.use("/blogs", blogRoute);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port, (err) => {
   if (err) throw err;
-  console.log("Server listening on port", port);
+  console.log(`Server listening on port http://localhost:${port}`);
 });
